52e64628c8eb1255d3f61a8b0373a14d1cafc9db
Your Name <you@example.com>
2020-11-07 17:31:53 +0330

    vnfghdtf

---
+++ Term 2/dr-_god11.py
@@ -0,0 +1,16 @@
+import turtle as tr
+import random as rnd
+
+tr.speed(500)
+for j in range(100):
+    tr.penup
+    x = position = rnd.randint(-200, 200)
+    y = position = rnd.randint(-150, 150)
+    tr.goto(x_position,y_position)
+    tr.pendown
+pen_size = rnd.randint(1, 5)
+tr.pensize(pen_size)
+for i in range(3):
+    tr.fd(150
+    tr.lt(120)
+    tr.done()    
