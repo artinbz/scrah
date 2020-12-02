3e53d2156ab0cbad129f9ec75a7019350e4e042a
artinbz <68778347+artinbz@users.noreply.github.com>
2020-12-02 07:53:15 -0800

    juy

--- Term 2/dr-_god24.py
+++ Term 2/dr-_god24.py
@@ -1,8 +1,17 @@
 number = input("Enter Number: ")
-
+counter = 0
+number = []
+s = 0 
 while number != '0':
-    print(number)
-    number = input("Enter Number: ")
+    if number.isdigit():
+        number.append(number) 
+    number = input("enter number: ")
     counter += 1
+for i in number:
+    s = s + int(n)
+print(number)
+print(s)
 print(f"you entered {counter} numbers.")
-3
+
+
+
---
+++ Term 2/ftf.py
@@ -0,0 +1,6 @@
+numbers = ['4', '5','3']
+s = 0
+for n in  numbers:
+     s = s + int(n)
+print (s)
+
