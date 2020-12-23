bc156d417878a233087d2bf41d907034278345c2
artinbz <68778347+artinbz@users.noreply.github.com>
2020-12-23 10:54:18 -0800

    Update dr-_god27.py

--- Term 2/dr-_god27.py
+++ Term 2/dr-_god27.py
@@ -7,4 +7,5 @@ data = requests.get(url)
 soup = BeautifulSoup(data.txt,'html.parser')
 p_tags = soup.find_all('p', class_="shortdesc removeEmoji")
 for p_tag in p_tags:
-    print(p_tag.txt)
+    print(p_tag.text)
+    
