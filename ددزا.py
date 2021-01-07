8a171e96812dc5d0a9da2bb2b02d60a4ab5cdd96
artinbz <68778347+artinbz@users.noreply.github.com>
2021-01-07 07:10:49 -0800

    ff

---
+++ ددزا.py
@@ -0,0 +1,15 @@
+# functions.py
+from . import pages
+
+
+def page_0():
+p = {
+'1': page_1,
+}
+print(pages.page_0)
+replay = input('replay: ')
+print("################")
+p[replay]()
+
+def page_1():
+print(pages.page_1)
