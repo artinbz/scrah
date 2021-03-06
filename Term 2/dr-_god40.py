1f1b2b84749feaf519e9c0f1531d2ba5c7a9692b
poulstar <poulstar@gmail.com>
2021-03-06 17:27:40 +0330

    gjthjyfgthfyjf

--- Term 2/dr-_god40.py
+++ Term 2/dr-_god40.py
@@ -1,6 +1,23 @@
 import tkinter as tk
-
+import tkinter.ttk as ttk
 import reg 
+
+
+def callback(a ,b ,c):
+    c = code.get()
+    e4.config(bg="red")
+    if c.isdigit():
+        if len(c == 10:)
+            e4.config(bg="green")
+        elif len(c) > 10:
+            code.set(c[:10])
+            e4.config(bg="green")
+    elif len(c) > 10:
+        code.set(c[:10])
+
+
+
+
 
 def register():
     reg.register(
@@ -13,29 +30,29 @@ def register():
 
 root = tk.Tk()
 
-tk.Label(root,text='name').grid(row=0, column=0)
-name = tk.Entry(root)
-name.grid(row=0, column=1)
+name = tk.stringvar()
+e1 = tk.Entry(root,textvariable=name)
+e1.grid(row=0, column=1)
 
-tk.Label(root, text='last').grid(row=1, column=0)
-last = tk.Entry(root)
-last.grid(row=1, column=1)
+last = tk.stringvar()
+e2 = tk.Entry(root,textvariable=last)
+e2.grid(row=1, column=1)
 
-tk.Label(root, text='birth date').grid(row=2, column=0)
-birth = tk.Entry(root,  )
-birth.grid(row=2, column=1)
+birth = tk.stringvar()
+e3 = tk.Entry(root,textvariable=last)
+e3.grid(row=2, column=1)
+
+code = tk.stringvar()
+code.trace("w", callback)
+e4 = tk.Entry(root, textvariable=code)
+e4.grid(row=3, column=1)
 
 
-tk.Label(root, text='id code').grid(row=3, column=0)
-code = tk.Entry(root,  )
-code.grid(row=3, column=1)
+fram = tk.Frame()
+fram.grid(row=4, column=0, columnspan=2)
 
-
-frame = tk.Frame(root)
-frame.grid(row=6, column=0, columnspan=2)
-
-tk.Button(frame,text='register', command=register).grid(row=0, column=0)
-tk.Button(frame,text='cancel', comman=root.destroy).grid(row=0, column=1)
+tk.Button(fram,text='register', command=register).grid(row=0, column=0)
+tk.Button(fram,text='cancel', comman=root.destroy).grid(row=0, column=1)
 
 root. mainloop()
 
